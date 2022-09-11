114f469b4f6c032468db26c42cfbb6aae6aa790f
Younes Dahmani <y.dahmaniyounes@gmail.com>
2022-09-11 13:12:21 +0200

    Realization of the series page

--- src/config/Requests.js
+++ src/config/Requests.js
@@ -14,7 +14,13 @@ const requests = {
 
   // TV
 
+  fetchTvTrending: `${baseURL}/trending/tv/week?api_key=${API_KEY}`,
   fetchTvPopular: `${baseURL}/tv/popular?api_key=${API_KEY}&language=en-US&page=1`,
+  fetchTvTopRated: `${baseURL}/tv/top_rated?api_key=${API_KEY}&language=en-US&page=1`,
+  fetchTalkShow: `${baseURL}/discover/tv?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false&with_watch_monetization_types=flatrate&with_status=0&with_type=5`,
+  fetchMiniSeries: `${baseURL}/discover/tv?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false&with_watch_monetization_types=flatrate&with_status=0&with_type=2`,
+  fetchTvDocumentary: `${baseURL}/discover/tv?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false&with_watch_monetization_types=flatrate&with_status=0&with_type=0`,
+
   fetchTvDrama: `${baseURL}/discover/tv?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&with_genres=18&include_null_first_air_dates=false&with_watch_monetization_types=flatrate&with_status=0&with_type=0`,
   fetchTvChildren: `${baseURL}/discover/tv?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&with_genres=10762&include_null_first_air_dates=false&with_watch_monetization_types=flatrate&with_status=0&with_type=0`,
 };
--- src/pages/Series.js
+++ src/pages/Series.js
@@ -1,11 +1,20 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-
+import Header from "../components/Banner";
+import Rows from "../components/Rows";
+import requests from "../config/Requests";
 const Series = () => {
   return (
     <div className="series">
       <Navbar />
-      <h1>Séries</h1>
+      <Header />
+      <Rows title="Trending this week" fetchUrl={requests.fetchTvTrending} />
+      <Rows title="Top Rated" fetchUrl={requests.fetchTvTopRated} />
+      <Rows title="For Children" fetchUrl={requests.fetchTvChildren} />
+      <Rows title="Popular TV Show" fetchUrl={requests.fetchTvPopular} />
+      <Rows title="Talk Show" fetchUrl={requests.fetchTalkShow} />
+      <Rows title="Mini Series" fetchUrl={requests.fetchMiniSeries} />
+      <Rows title="Documentary" fetchUrl={requests.fetchTvDocumentary} />
     </div>
   );
 };
